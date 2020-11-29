77a3ad9c14af24dc08e55233af0422c6d22f4b73
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2020-11-30 03:30:57 +0530

    Create RobotProgram.c

---
+++ RobotProgram.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	
+	int t;
+	scanf("%d", &t);
+	
+	while(t--) {
+		
+		int x, y;
+		scanf("%d %d", &x, &y);
+		
+		if(x == y)
+			printf("%d\n", 2*x);
+		else if ( x < y) 
+			printf("%d\n", 2*y - 1);
+		else if ( x > y  )
+			printf("%d\n", 2*x - 1);
+		else 
+			continue;
+	}
+	return 0;
+}
