a9fcfad00b707b5d420d34715f8e0766befe9870
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2020-11-30 03:32:13 +0530

    Update RobotProgram.c

--- RobotProgram.c
+++ RobotProgram.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 void main() {
-	
-	int t;
-	scanf("%d", &t);
-	
-	while(t--) {
-		
-		int x, y;
-		scanf("%d %d", &x, &y);
-		
-		if(x == y)
-			printf("%d\n", 2*x);
-		else if ( x < y) 
-			printf("%d\n", 2*y - 1);
-		else if ( x > y  )
-			printf("%d\n", 2*x - 1);
-		else 
-			continue;
-	}
+    
+    int t;
+    scanf("%d", &t);
+    
+    while(t--) {
+        
+        int x, y;
+        scanf("%d %d", &x, &y);
+        
+        if(x == y)
+            printf("%d\n", 2*x);
+        else if ( x < y) 
+            printf("%d\n", 2*y - 1);
+        else if ( x > y  )
+            printf("%d\n", 2*x - 1);
+        else 
+            continue;
+    }
 }
